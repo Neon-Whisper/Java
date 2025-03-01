70c07778159866baf6e27348f0bcb329b2edc80d
fuzhengwei <184172133@qq.com>
2025-03-01 16:31:28 +0800

    feat: 第8节：Git仓库代码库解析到知识库

--- xfg-dev-tech-api/src/main/java/cn/bugstack/xfg/dev/tech/api/IRAGService.java
+++ xfg-dev-tech-api/src/main/java/cn/bugstack/xfg/dev/tech/api/IRAGService.java
@@ -3,6 +3,7 @@ package cn.bugstack.xfg.dev.tech.api;
 import cn.bugstack.xfg.dev.tech.api.response.Response;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IRAGService {
@@ -10,5 +11,7 @@ public interface IRAGService {
     Response<List<String>> queryRagTagList();
 
     Response<String> uploadFile(String ragTag, List<MultipartFile> files);
+
+    Response<String> analyzeGitRepository(String repoUrl, String userName, String token) throws Exception;
 
 }
