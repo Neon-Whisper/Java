a1489314b6cedffc1e8bc390064b29e70bb3d94a
NeonWhisper <neon1077881361@outlook.com>
2025-11-12 22:30:52 +0800

    5-RAG

--- java_2025/ai-rag-knowledge/dev-tech-api/src/main/java/com/neon/dev/tech/api/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @description
- * @author Fuzhengwei bugstack.cn @小傅哥
- * @create 2025-03-01 11:07
- */
-package com.neon.dev.tech.api;
--- java_2025/ai-rag-knowledge/dev-tech-trigger/src/main/java/com/neon/dev/tech/trigger/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @description
- * @author Fuzhengwei bugstack.cn @小傅哥
- * @create 2025-03-01 11:08
- */
-package com.neon.dev.tech.trigger;
